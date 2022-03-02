e2c239f345945d2ccd339a4c9e84d88db3f444a3
Levi Starrett <levi@roxsoftware.com>
2022-03-01 22:34:22 -0500

    proof of concept with Environment

--- utils/Environment_OOA/custom/custom.cmake
+++ utils/Environment_OOA/custom/custom.cmake
@@ -1,4 +1,5 @@
 #
 # UK Crown Copyright (c) 2016. All Rights Reserved
 #
-target_sources( Environment_interface PRIVATE ${CMAKE_CURRENT_LIST_DIR}/Environment_services.cc )
+target_sources( Environment_interface PRIVATE ${CMAKE_CURRENT_LIST_DIR}/Environment_services.cc ${CMAKE_CURRENT_LIST_DIR}/Environment_JNI.cc )
+target_include_directories( Environment_interface PRIVATE /usr/lib/jvm/java-8-openjdk-arm64/include /usr/lib/jvm/java-8-openjdk-arm64/include/linux )
