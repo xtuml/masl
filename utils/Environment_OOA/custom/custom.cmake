07b89bfc85243d73836c3560c8d91df88fd6d928
Levi Starrett <levi@roxsoftware.com>
2022-03-02 10:57:26 -0500

    fix JAVA_HOME issue

--- utils/Environment_OOA/custom/custom.cmake
+++ utils/Environment_OOA/custom/custom.cmake
@@ -2,4 +2,4 @@
 # UK Crown Copyright (c) 2016. All Rights Reserved
 #
 target_sources( Environment_interface PRIVATE ${CMAKE_CURRENT_LIST_DIR}/Environment_services.cc ${CMAKE_CURRENT_LIST_DIR}/Environment_JNI.cc )
-target_include_directories( Environment_interface PRIVATE /usr/lib/jvm/java-8-openjdk-arm64/include /usr/lib/jvm/java-8-openjdk-arm64/include/linux )
+target_include_directories( Environment_interface PRIVATE $ENV{JAVA_HOME}/include $ENV{JAVA_HOME}/include/linux )
