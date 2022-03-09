563990986df39b0f019fe5d32c6e56e3e26fe884
Levi Starrett <levi@roxsoftware.com>
2022-03-09 10:28:15 -0500

    Add calculator example; modify petrol to be buildable with docker images

---
+++ examples/petrol/build-masl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+WINPTY=`which winpty || echo ""`
+MASLMC="$WINPTY docker run -v /$PWD:/workspace levistarrett/masl-compiler -skiptranslator Sqlite"
+$MASLMC -domainpath //root/masl/utils -mod PSC_OOA/PSC.mod -test -output src/PSC_OOA && \
+$MASLMC -domainpath //root/masl/utils:PSC_OOA -prj PETROL_PROC/PETROL_PROC.prj -output src/PETROL_PROC && \
+$WINPTY docker run -it -v /$PWD:/workspace levistarrett/masl-build
