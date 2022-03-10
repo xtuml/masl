8e92130750166cb5175c364a3f29637d304c3be6
Levi Starrett <levi@roxsoftware.com>
2022-03-09 21:18:22 -0500

    Fix examples to remove docker images after run

--- examples/calculator/build-masl.sh
+++ examples/calculator/build-masl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WINPTY=`which winpty || echo ""`
-MASLMC="$WINPTY docker run -v /$PWD:/workspace levistarrett/masl-compiler -skiptranslator Sqlite"
+MASLMC="$WINPTY docker run --rm -v /$PWD:/workspace levistarrett/masl-compiler -skiptranslator Sqlite"
 $MASLMC -domainpath //root/masl/utils -mod ALU_OOA/ALU.mod -test -output src/ALU_OOA && \
 $MASLMC -domainpath //root/masl/utils:ALU_OOA -prj calculator_proc/calculator.prj -output src/calculator_proc && \
-$WINPTY docker run -it -v /$PWD:/workspace levistarrett/masl-build
+$WINPTY docker run --rm -it -v /$PWD:/workspace levistarrett/masl-build
