e460d1acd4fdcb194620acbc80174a301548ee7a
Levi Starrett <levi@roxsoftware.com>
2022-10-13 15:06:48 -0400

    Update examples to work with latest docker images

--- examples/petrol/build-masl.sh
+++ examples/petrol/build-masl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WINPTY=`which winpty || echo ""`
-MASLMC="$WINPTY docker run --rm -v /$PWD:/workspace levistarrett/masl-compiler -skiptranslator Sqlite"
-$MASLMC -domainpath //root/masl/utils -mod PSC_OOA/PSC.mod -test -output src/PSC_OOA && \
-$MASLMC -domainpath //root/masl/utils:PSC_OOA -prj PETROL_PROC/PETROL_PROC.prj -output src/PETROL_PROC && \
-$WINPTY docker run --rm -it -v /$PWD:/workspace levistarrett/masl-build
+MASLMC="$WINPTY docker run --rm -v /$PWD:/root levistarrett/masl-compiler -skiptranslator Sqlite"
+$MASLMC -domainpath //opt/masl/lib/masl -mod PSC_OOA/PSC.mod -test -output src/PSC_OOA && \
+$MASLMC -domainpath //opt/masl/lib/masl:PSC_OOA -prj PETROL_PROC/PETROL_PROC.prj -output src/PETROL_PROC && \
+$WINPTY docker run --rm -it -v /$PWD:/root levistarrett/masl-build
