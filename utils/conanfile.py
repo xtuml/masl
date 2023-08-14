2655753ae52c974281851d1e6662f8feee7d5322
Levi Starrett <levi@roxsoftware.com>
2023-08-14 14:55:58 -0400

    Change JSON validation to use env var for metaschema location

--- utils/conanfile.py
+++ utils/conanfile.py
@@ -15,6 +15,10 @@
 #  ----------------------------------------------------------------------------
 
 import conan
+import os
+
+from conan.tools.files import copy
+
 
 class ConanFile(conan.ConanFile):
     name = "masl_utils"
@@ -24,9 +28,18 @@ class ConanFile(conan.ConanFile):
     python_requires = 'masl_conan/0.1@xtuml/stable'
     python_requires_extend = 'masl_conan.MaslConanHelper'
 
-    exports_sources= ( "*_OOA/*")
+    exports_sources = "*_OOA/*"
 
     def requirements(self):
         self.requires(f"masl_core/{self.version}@xtuml/stable")
         self.requires("libuuid/1.0.3")
         self.tool_requires(f"masl_codegen/{self.version}@xtuml/stable")
+
+    def package(self):
+        super().package()
+        res_dir = os.path.join(self.package_folder, 'res')
+        copy(self, 'json-metaschema.json', src=os.path.join(self.source_folder, 'JSONValidation_OOA', 'custom'), dst=res_dir)
+
+    def package_info(self):
+        super().package_info()
+        self.runenv_info.define_path('JSON_META_SCHEMA_PATH', os.path.join(self.package_folder, 'res', 'json-metaschema.json'))
