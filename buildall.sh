3c153558d45bf00ce7649cd8f85060aed9839edd
Levi Starrett <levi@roxsoftware.com>
2024-02-28 13:18:21 -0500

    Change publish script to be more flexible

--- buildall.sh
+++ buildall.sh
@@ -4,14 +4,16 @@ set -e
 base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 function masl-dev {
-	docker compose -f ${base_dir}/docker/docker-compose.yml run -e ARTIFACTORY_USERNAME=${ARTIFACTORY_USERNAME} -e ARTIFACTORY_TOKEN=${ARTIFACTORY_TOKEN} -e MASL_VERSION=$(git describe --tags) --rm -v $PWD:/work masl-dev "$@"
+	docker compose -f ${base_dir}/docker/docker-compose.yml run -e ARTIFACTORY_USERNAME=${ARTIFACTORY_USERNAME} -e ARTIFACTORY_TOKEN=${ARTIFACTORY_TOKEN} --rm -v $PWD:/work masl-dev "$@"
 }
 
-(cd conan-helper && masl-dev conan-publish)
-(cd conan-helper && masl-dev conan-publish)
-(cd core-cpp && masl-dev conan-publish)
-(cd core-java && masl-dev conan-publish)
-(cd utils && masl-dev conan-publish)
-(cd inspector && masl-dev conan-publish)
-# (cd examples/petrol && masl-dev  conan-publish) LPS not working
-(cd examples/calculator && masl-dev conan-publish)
+VERSION=$(git describe --tags)
+
+(cd conan-helper && masl-dev conan-publish --version=${VERSION})
+(cd conan-helper && masl-dev conan-publish --version=${VERSION})
+(cd core-cpp && masl-dev conan-publish --version=${VERSION})
+(cd core-java && masl-dev conan-publish --version=${VERSION})
+(cd utils && masl-dev conan-publish --version=${VERSION})
+(cd inspector && masl-dev conan-publish --version=${VERSION})
+# (cd examples/petrol && masl-dev  conan-publish --version=${VERSION}) LPS not working
+(cd examples/calculator && masl-dev conan-publish --version=${VERSION})
