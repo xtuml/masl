12f54af59052641fedae0167ed3eb551a0c21668
Tristan Pye <5959807+tristanpye@users.noreply.github.com>
2025-03-07 14:23:52 +0000

    Add production conan remote

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker run --rm  -v conan-cache:/conan-cache -v $PWD:/work levistarrett/masl-dev:latest ./build-all.sh xtuml ${ARTIFACTORY_USERNAME} ${ARTIFACTORY_TOKEN}
+repo=${1:=testing}
+
+docker run --rm  -v conan-cache:/conan-cache -v $PWD:/work levistarrett/masl-dev:latest ./build-all.sh ${repo} ${ARTIFACTORY_USERNAME} ${ARTIFACTORY_TOKEN}
 
