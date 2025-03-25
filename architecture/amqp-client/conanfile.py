a285392b30a4a8d91430309a5bc1d927357a23b5
Levi Starrett <levi@roxsoftware.com>
2025-03-25 13:40:59 -0400

    Bump versions for release

--- architecture/amqp-client/conanfile.py
+++ architecture/amqp-client/conanfile.py
@@ -5,7 +5,7 @@ from conan.errors import ConanInvalidConfiguration
 
 class ConanFile(conan.ConanFile):
     name = "xtuml_amqp_client"
-    version = "1.0"
+    version = "1.1"
     user = "xtuml"
 
     package_type = "shared-library"
