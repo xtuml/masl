856e913f70277816e9591edf18f82539ad2e1a19
Levi Starrett <levi@roxsoftware.com>
2025-03-31 11:17:16 -0400

    final version bump before release

--- masl-conan/conanfile.py
+++ masl-conan/conanfile.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 class ConanFile(conan.ConanFile):
     name = 'xtuml_masl_conan'
-    version = '5.1-prerelease7'
+    version = '5.1'
     user = 'xtuml'
 
     package_type = 'python-require'
