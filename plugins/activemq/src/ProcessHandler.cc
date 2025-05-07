de3e92919fe8ed1bf7fcdc5ea44e603d46fbef40
Levi Starrett <levi@roxsoftware.com>
2025-05-07 11:24:41 -0400

    Add completion handler to propagate exceptions back to the main part of the process

--- plugins/activemq/src/ProcessHandler.cc
+++ plugins/activemq/src/ProcessHandler.cc
@@ -7,7 +7,6 @@
 #include "swa/Process.hh"
 
 #include <asio/co_spawn.hpp>
-#include <asio/detached.hpp>
 #include <asio/ssl.hpp>
 #include <uuid/uuid.h>
 
@@ -75,51 +74,47 @@ namespace InterDomainMessaging {
             asio::co_spawn(
                 SWA::Process::getInstance().getIOContext().get_executor(),
                 [this]() -> asio::awaitable<void> {
-                    try {
+                    BrokerConfig config;
 
-                        BrokerConfig config;
+                    // setup ssl options
+                    config.ssl.certificate_file = SWA::CommandLine::getInstance().getOption(SslCertOption);
+                    config.ssl.private_key_file = SWA::CommandLine::getInstance().getOption(SslKeyOption);
+                    config.ssl.ca_file = SWA::CommandLine::getInstance().getOption(SslCertAuthOption);
+                    config.ssl.password = SWA::CommandLine::getInstance().getOption(SslPasswordOption);
 
-                        // setup ssl options
-                        config.ssl.certificate_file = SWA::CommandLine::getInstance().getOption(SslCertOption);
-                        config.ssl.private_key_file = SWA::CommandLine::getInstance().getOption(SslKeyOption);
-                        config.ssl.ca_file = SWA::CommandLine::getInstance().getOption(SslCertAuthOption);
-                        config.ssl.password = SWA::CommandLine::getInstance().getOption(SslPasswordOption);
+                    // setup broker options
+                    config.hostname = SWA::CommandLine::getInstance().getOption(BrokerOption);
+                    config.port = SWA::CommandLine::getInstance().getOption(PortNoOption, config.ssl.use_ssl() ? "5671" : "5672");
+                    config.username = SWA::CommandLine::getInstance().getOption(UsernameOption, "admin");
+                    config.password = SWA::CommandLine::getInstance().getOption(PasswordOption, "admin");
 
-                        // setup broker options
-                        config.hostname = SWA::CommandLine::getInstance().getOption(BrokerOption);
-                        config.port = SWA::CommandLine::getInstance().getOption(PortNoOption, config.ssl.use_ssl() ? "5671" : "5672");
-                        config.username = SWA::CommandLine::getInstance().getOption(UsernameOption, "admin");
-                        config.password = SWA::CommandLine::getInstance().getOption(PasswordOption, "admin");
-
-                        // create connection
-                        if (config.ssl.use_ssl()) {
-                            auto ssl_ctx = config.ssl.context();
-                            conn = amqp_asio::Connection::create_amqps(getName(), SWA::Process::getInstance().getIOContext().get_executor(), ssl_ctx);
-                        } else {
-                            conn = amqp_asio::Connection::create_amqp(getName(), SWA::Process::getInstance().getIOContext().get_executor());
-                        }
-                        co_await conn.open(
-                            amqp_asio::ConnectionOptions()
-                                .hostname(config.hostname)
-                                .port(config.port)
-                                .sasl_options(amqp_asio::SaslOptions().authname(config.username).password(config.password))
-                        );
-                        log.debug("Connection open");
-
-                        // open a session
-                        session = co_await conn.open_session();
-                        log.debug("Session open");
-
-                        initialised = true;
-                        initialisedCond.notify();
-
-                    } catch (std::bad_variant_access &e) {
-                        throw std::system_error(make_error_code(std::errc::bad_message));
-                    } catch (const std::system_error &e) {
-                        fmt::println("Error: {}", e.code().message());
+                    // create connection
+                    if (config.ssl.use_ssl()) {
+                        auto ssl_ctx = config.ssl.context();
+                        conn = amqp_asio::Connection::create_amqps(getName(), SWA::Process::getInstance().getIOContext().get_executor(), ssl_ctx);
+                    } else {
+                        conn = amqp_asio::Connection::create_amqp(getName(), SWA::Process::getInstance().getIOContext().get_executor());
                     }
+                    co_await conn.open(
+                        amqp_asio::ConnectionOptions()
+                            .hostname(config.hostname)
+                            .port(config.port)
+                            .sasl_options(amqp_asio::SaslOptions().authname(config.username).password(config.password))
+                    );
+                    log.debug("Connection open");
+
+                    // open a session
+                    session = co_await conn.open_session();
+                    log.debug("Session open");
+
+                    initialised = true;
+                    initialisedCond.notify();
                 },
-                asio::detached
+                [](std::exception_ptr eptr) {
+                    if (eptr) {
+                        std::rethrow_exception(eptr);
+                    }
+                }
             );
         }
 
