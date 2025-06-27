36557ccc248f27a81c9cd3c2b9e2547b31feb5b4
Levi Starrett <levi@roxsoftware.com>
2025-06-27 13:54:42 -0400

    Move initialisation code to IDM implementation plugins

--- plugins/idm/src/ProcessHandler.cc
+++ plugins/idm/src/ProcessHandler.cc
@@ -7,21 +7,8 @@ namespace InterDomainMessaging {
         return getSingleton();
     }
 
+    bool ProcessHandler::hasImplementation() {
+        return singletonRegistered();
+    }
+
 } // namespace InterDomainMessaging
-
-namespace {
-
-    // Load up the domain handlers once the process has initialised and domains are
-    // known.
-    void loadLibs() {
-        SWA::Process::getInstance().loadDynamicLibraries("idm", "_if", false);
-    }
-
-    bool initialise() {
-        SWA::Process::getInstance().registerInitialisedListener(&loadLibs);
-        return true;
-    }
-
-    bool initialised = initialise();
-
-} // namespace
